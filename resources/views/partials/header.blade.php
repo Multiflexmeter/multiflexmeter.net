3d062bbf489f16bb138d4b2a674cef4cb4349f1e
Tim van Osch <timvosch@pollex.nl>
2020-06-18 22:09:19 +0200

    Adjust navbar dimensions

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -2,7 +2,7 @@
 <header class="relative border-b-1">
   <!-- Branding -->
   <div class="branding md:absolute md:h-full flex flex-row items-center justify-between h-12">
-    <a href="{{ home_url('/') }}" class="block h-6 pl-6">
+    <a href="{{ home_url('/') }}" class="block h-5 pl-6">
       <img src="@asset('images/logo-liggend.png')" alt="{{ get_bloginfo('name', 'display') }}" class="h-full hidden md:block">
       <img src="@asset('images/logo-liggend-wit.png')" alt="{{ get_bloginfo('name', 'display') }}" class="h-full block md:hidden">
     </a>
