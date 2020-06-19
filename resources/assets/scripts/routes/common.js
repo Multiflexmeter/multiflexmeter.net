aeaf5c9b5f6deb472670c4c5c560e4f7fdc4ddb5
Tim van Osch <timvosch@pollex.nl>
2020-06-19 16:31:01 +0200

    Only scroll sub-menus on mobile

--- resources/assets/scripts/routes/common.js
+++ resources/assets/scripts/routes/common.js
@@ -3,22 +3,24 @@ export default {
     // JavaScript to be fired on all pages
   },
   finalize() {
-    // JavaScript to be fired on all pages, after page specific JS is fired
-    $('header nav .has-submenu > .nav-link').on('click', (event) => {
-      event.preventDefault();
-      const hasSubmenuDiv = $(event.target).parent();
-      hasSubmenuDiv.children('.submenu').first().slideToggle({
-        queue: false,
+    if (window.innerWidth <= 640) {
+      // JavaScript to be fired on all pages, after page specific JS is fired
+      $('header nav .has-submenu > .nav-link').on('click', (event) => {
+        event.preventDefault();
+        const hasSubmenuDiv = $(event.target).parent();
+        hasSubmenuDiv.children('.submenu').first().slideToggle({
+          queue: false,
+        });
       });
-    });
 
-    $('.collapse-button').on('click', (event) => {
-      console.log(event);
+      $('.collapse-button').on('click', (event) => {
+        console.log(event);
 
-      const collapseTarget = $(event.currentTarget).attr('data-collapse');
-      $(collapseTarget).slideToggle({
-        queue: false,
+        const collapseTarget = $(event.currentTarget).attr('data-collapse');
+        $(collapseTarget).slideToggle({
+          queue: false,
+        });
       });
-    });
+    }
   },
 };
